e2e35272ce568bf2c5baea41205b47976a5ad8a9
muhammad <mukhamed.saidov.2002@mail.ru>
2023-06-16 23:57:13 +0400

    4 задание

---
+++ 4_ex/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear(); 
+Console.Write("num1: "); 
+int num1 = int.Parse(Console.ReadLine()); 
+Console.Write("num2: "); 
+int num2 = int.Parse(Console.ReadLine()); 
+Console.Write("num3: "); 
+int num3 = int.Parse(Console.ReadLine()); 
+int max = num1; 
+ 
+if (num1 < num2) max = num2; 
+if (num2 < num3) max = num3; 
+ 
+Console.WriteLine(max);
