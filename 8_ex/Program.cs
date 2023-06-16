81c816fd381d1cf3827cc3e1fa17c9781647c471
muhammad <mukhamed.saidov.2002@mail.ru>
2023-06-17 00:57:15 +0400

    8 задание

---
+++ 8_ex/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Показать четные числа от 1 до {n}");
+void printEvenNumbers(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
+printEvenNumbers(n);
+Console.WriteLine("\n");
+    
