4341b1e3e4cf3751a42ba1f4384ed34a0736cdf6
muhammad <mukhamed.saidov.2002@mail.ru>
2023-06-17 22:41:05 +0400

    15

---
+++ 15_ex/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num > 5 && num < 8)
+{
+    Console.WriteLine($"{num} -> да, выходной");
+    return;
+}
+if (num > 0 && num < 6)
+{
+    Console.WriteLine($"{num} -> нет(");
+    return;
+}
+else 
+{
+    Console.WriteLine($"{num} -> данные не коректны");
+}
