9842c2f5c06480d1a725281253cd928983586288
muhammad <mukhamed.saidov.2002@mail.ru>
2023-06-17 21:57:49 +0400

    13

---
+++ 13_ex/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+int ThirdNum(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return (number % 10);
+}
+bool AnoNum(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет!");
+        return true;
+    }
+    return false;
+}
+
+int number = Prompt("Введите число: ");
+if (AnoNum(number) != true)
+{
+    Console.WriteLine(ThirdNum(number));
+}
