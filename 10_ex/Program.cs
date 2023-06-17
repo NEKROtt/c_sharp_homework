82871bb7dc21b11981ab3e837396b4bec98ff3fc
muhammad <mukhamed.saidov.2002@mail.ru>
2023-06-17 20:32:12 +0400

    10

---
+++ 10_ex/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num=int.Parse(Console.ReadLine());
+
+if (num < 100 || num >= 1000)
+{
+    Console.WriteLine("Это не трехзначное число!");
+    return;
+}
+int secondNum = num / 10 % 10;
+Console.WriteLine($"Вторая цифра -> {secondNum}");
