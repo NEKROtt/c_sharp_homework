8a3a85e1472a942b11b02196d8c3fb40e38c7de4
muhammad <mukhamed.saidov.2002@mail.ru>
2023-07-10 15:17:11 +0400

    27

---
+++ 27_ex/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.Clear();
+
+int First(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int Second(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = First("Введите число: ");
+Console.WriteLine($"Сумма всех цифр в числе {number} равна {Second(number)}");
+
+
